950d484b6f99fa8c4216b7a3778926014b4d7dbb
ju-kkim <wndus8198@naver.com>
2022-04-18 17:59:32 +0900

    feat: webpack 설정

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import resetCss from '../sass/reset.scss';
+import styleCss from '../sass/style.scss';
+import test from './test.js';
+
+Console.log(test);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new CopyPlugin({
+      patterns: [{ from: './src/img', to: './src/img' }],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 8080,
+  },
+};
